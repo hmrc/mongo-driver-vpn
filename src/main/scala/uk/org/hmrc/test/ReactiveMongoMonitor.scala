7aad994a21d8d0420c23944f04c8e9874b15530d
Waldemar Wosiński <kastoestoramadus@gmail.com>
2017-01-24 12:03:05 +0000

    Optimize imports fix

--- src/main/scala/uk/org/hmrc/test/ReactiveMongoMonitor.scala
+++ src/main/scala/uk/org/hmrc/test/ReactiveMongoMonitor.scala
@@ -4,6 +4,8 @@ import reactivemongo.api.MongoConnection
 import reactivemongo.api.collections.bson.BSONCollection
 import reactivemongo.api.commands.WriteResult
 import reactivemongo.bson.BSONDocument
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
 import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Random, Success}
